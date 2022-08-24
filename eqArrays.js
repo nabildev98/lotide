a9eefc74fef250653c693d10fba94f918537c676
vagrant <nabilahmed98@hotmail.com>
2022-08-24 20:41:44 +0000

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed. ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion failed. ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
