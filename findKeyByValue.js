1f4795d153aa4cde3e04805e30e7ac06328f21e3
vagrant <nabilahmed98@hotmail.com>
2022-08-25 19:43:27 +0000

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed. ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion failed. ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  const keys = object.keys;
+  for (let key in object) {
+    if (object[key] === value) return key;
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+// findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine")
