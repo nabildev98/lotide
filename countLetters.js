ad2c2af9b0dbe6b6296a6222a76507861478a28a
vagrant <nabilahmed98@hotmail.com>
2022-08-25 14:21:47 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed. ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion failed. ${actual} !== ${expected}`);
+  }
+};
+
+function countLetters(string) {
+  const count = {};
+  string.split("").forEach((char) => {
+    count[char] = count[char] ? count[char] + 1 : 1;
+  });
+  return count;
+}
+console.log(countLetters("lighthouse in the house"));
