440b0ecddf7bad5a44c244083c9fc091bb4c8896
vagrant <nabilahmed98@hotmail.com>
2022-08-26 21:51:50 +0000

    takeUntil

--- practise.js
+++ practise.js
@@ -1,13 +1,3 @@
-const letterPositions = function (sentence) {
-  const results = {};
-  for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " ") {
-      if (results[sentence[i]]) {
-        results[sentence[i]].push(i);
-      } else {
-        results[sentence[i]] = [i];
-      }
-    }
-    return results;
-  }
-};
+const names = [];
+names.push("Jordan");
+console.log(names);
---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("Equal!");
+  } else {
+    console.log("Not Equal");
+  }
+};
+
+const takeUntil = function (array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      array.splice(i);
+      return array;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
