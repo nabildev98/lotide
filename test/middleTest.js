973591d916d1203edc0d50733f5a49164b95cb52
vagrant <nabilahmed98@hotmail.com>
2022-08-30 20:01:44 +0000

    tail & middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,23 +1,15 @@
 const middle = require("../middle");
+const assertEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-// const eqArrays = function (arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(middle(["5"]), "5");
+  });
+});
 
-// const assertArraysEqual = function (arr1, arr2) {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log("Equal!");
-//   } else {
-//     console.log("Not Equal");
-//   }
-// };
 console.log(middle([1, 2, 3, 4, 5, 6]));
 console.log(middle([1, 2, 3]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,15 @@
 const assertEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
+const tail = require("../tail");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(["5"]), "5");
+  });
+});
 
 // TEST CODE
 assertEqual("Labs", "Labs");
